457d7cf7c59670e1d2dc4c312c5ee904167edc74
Lolo Escobar. <bylectroyt@gmail.com>
2025-02-13 10:25:32 +0100

    minor fix

--- app/Console/Commands/TmdbImport.php
+++ app/Console/Commands/TmdbImport.php
@@ -39,7 +39,14 @@ class TmdbImport extends Command
         $this->info("Iniciando la importación de datos de TMDB");
 
         // Obtén la fecha de hoy en el formato "m_d_Y", por ejemplo "02_11_2025"
-        $date = now()->format('m_d_Y');
+
+        $date = now();
+
+        if ($date->hour < 11) {
+            $date = $date->subDay();
+        }
+
+        $requiredFormatDate = $date->format('m_d_Y');
 
         // Definir la configuración de cada entidad con el "template" del nombre
         $entities = [
@@ -78,7 +85,7 @@ class TmdbImport extends Command
 
         foreach ($entities as $entityName => $data) {
             // Reemplaza la fecha en el nombre del archivo
-            $fileName = str_replace('MM_DD_YYYY', $date, $data['file']);
+            $fileName = str_replace('MM_DD_YYYY', $requiredFormatDate, $data['file']);
             // Construir la URL completa
             $url = "https://files.tmdb.org{$basePath}/{$fileName}";
             $this->info("Procesando {$entityName}: descargando {$url}");
@@ -103,7 +110,6 @@ class TmdbImport extends Command
                         $record = json_decode($line, true);
                         $this->info("{$record['id']} to {$data['model']}");
                         if ($record) {
-                            $record["original_title"] = Str::limit($record['original_title'], 250);
                             // Inserta o actualiza el registro según el id
 
                             $data['model']::updateOrCreate(
