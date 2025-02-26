8e688cf426bb9f736d61d421cb140999661a0602
Lolo Escobar. <bylectroyt@gmail.com>
2025-02-26 16:19:02 +0100

    up

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\TmdbServiceProvider::class,
+    Illuminate\Redis\RedisServiceProvider::class
 ];
