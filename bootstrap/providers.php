6b4f7fdf7ff6774b629c867ca6c333b84e1f862a
Lolo Escobar. <bylectroyt@gmail.com>
2025-03-18 13:17:30 +0100

    Upgrading packages

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,8 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     App\Providers\TmdbServiceProvider::class,
     Illuminate\Redis\RedisServiceProvider::class,
-    Laravel\Sanctum\SanctumServiceProvider::class
+    Laravel\Sanctum\SanctumServiceProvider::class,
 ];
