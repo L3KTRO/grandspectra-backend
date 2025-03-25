819353c1394e2d5483a3f8f731c0b107475c62af
Lolo Escobar. <bylectroyt@gmail.com>
2025-03-25 10:35:31 +0100

    fix

--- app/Services/Tmdb/Client/TV.php
+++ app/Services/Tmdb/Client/TV.php
@@ -424,7 +424,7 @@ class TV
                     'tv_id' => $this->data['id'],
                     'person_id' => $person['id'],
                     'occupation_id' => Occupation::ACTOR->value,
-                    'character' => Str::limit($role['character'] ?? '', 255),
+                    'character' => Str::limit($role['character'] ?? '', 250),
                     'order' => $person['order'] ?? null
                 ];
             }
