8094a45ff89eb7778186a790726538ca164edc81
Lolo Escobar. <bylectroyt@gmail.com>
2025-04-07 17:41:55 +0200

    crontab

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,8 @@ php artisan config:cache
 php artisan view:cache
 php artisan route:cache
 
+(crontab -l ; echo "* * * * * cd /var/www && php artisan schedule:run >> /dev/null 2>&1")| crontab -
+
 # Asegurar permisos finales
 chown -R www-data:www-data /var/www/storage
 
